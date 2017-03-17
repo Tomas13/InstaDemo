ff94ef1a21ad0ecfb2e44a0c0bcacc1b7eca535a
Zhangali Bidaibekov <tomas.mapoter@gmail.com>
2017-03-17 18:30:55 +0600

    start MainActivity from login

--- app/src/main/java/com/example/root/shit/view/LoginActivity.java
+++ app/src/main/java/com/example/root/shit/view/LoginActivity.java
@@ -1,19 +1,33 @@
 package com.example.root.shit.view;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.root.shit.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_signin)
+    Button btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+        btnSignIn.setOnClickListener(v -> goToMainActivity());
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
     }
 
     @Override
